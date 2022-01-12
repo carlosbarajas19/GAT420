6ecbcce3acd5827b9783afe760e2d8ac113a95f2
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-01-11 19:04:55 -0700

    Enemy Movement stuffz

---
+++ .bash_history
@@ -0,0 +1,11 @@
+apt intsall git
+apt install git
+cd D:\Unity Projects\GAT420
+cd \Unity Projects\GAT420
+cd
+cd \users
+cd \unity projects
+cd \Unity-Projects
+cd \UnityProjects
+cd \Unity Projects
+cd \Unity_Projects
---
+++ Assets/Scripts/AutonomousAgent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutonomousAgent : Agent
+{
+    [SerializeField] Perception perception;
+    [SerializeField] Steering steering;
+
+    public float maxSpeed;
+    public float maxForce;
+
+
+    public Vector3 velocity { get; set; } = Vector3.zero;
+
+    void Update()
+    {
+        Vector3 acceleration = Vector3.zero;
+        
+
+
+        GameObject[] gameObjects = perception.GetGameObjects();
+        if(gameObjects.Length != 0)
+        {
+            Debug.DrawLine(transform.position, gameObjects[0].transform.position);
+
+            Vector3 force = steering.Flee(this, gameObjects[0]);
+            acceleration += force;
+        }
+
+        velocity += acceleration * Time.deltaTime;
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
+
+        if(velocity.sqrMagnitude > 0.1f)
+        {
+            transform.rotation = Quaternion.LookRotation(velocity);
+        }
+    }
+}
