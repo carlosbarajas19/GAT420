14061010975300e12406cbf608bf991108403090
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-02-17 21:00:32 -0700

    Utility Agent stuff

--- Assets/Scripts/UtilityAgent/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgent/UtilityAgent.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class UtilityAgent : Agent
 {
+    Need[] needs;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        needs = GetComponentsInChildren<Need>(); 
     }
 
     // Update is called once per frame
@@ -15,4 +18,19 @@ public class UtilityAgent : Agent
     {
         
     }
+
+    private void OnGUI()
+    {
+        Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
+
+        GUI.color = Color.black;
+        int offset = 0;
+        foreach (var need in needs)
+        {
+            GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), need.type.ToString() + ": " + need.value);
+            offset += 20;
+        }
+        //GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), mood.ToString());
+    }
+
 }
