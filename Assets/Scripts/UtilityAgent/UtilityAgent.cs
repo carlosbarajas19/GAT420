afa40efd76fe75f0992528b3f415009d24249e32
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-02-18 12:05:53 -0700

    Slider stuff

--- Assets/Scripts/UtilityAgent/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgent/UtilityAgent.cs
@@ -6,20 +6,40 @@ public class UtilityAgent : Agent
 {
     Need[] needs;
 
+    public MeterUI meter;
+
+    public float happiness
+    {
+        get
+        {
+            float totalMovive = 0;
+            foreach(var need in needs)
+            {
+                totalMovive += need.motive;
+            }
+
+            return 1 - (totalMovive / needs.Length);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        needs = GetComponentsInChildren<Need>(); 
+        needs = GetComponentsInChildren<Need>();
+
+        meter.text.text = "";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //animator.SetFloat("speed", movement.velocity.magnitude);
+        meter.slider.value = happiness;
+        meter.worldPosition = transform.position + (Vector3.up * 4);
     }
 
-    private void OnGUI()
+    /*private void OnGUI()
     {
         Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
 
@@ -27,10 +47,10 @@ public class UtilityAgent : Agent
         int offset = 0;
         foreach (var need in needs)
         {
-            GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), need.type.ToString() + ": " + need.value);
+            GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), need.type.ToString() + ": " + need.motive);
             offset += 20;
         }
         //GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), mood.ToString());
-    }
+    }*/
 
 }
