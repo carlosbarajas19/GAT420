cfc538792c01adb8bb413a4cabe28f748d7aec6f
carlosbarajas19 <cbarajas@student.neumont.edu>
2022-03-02 22:31:03 -0700

    Utility Agent stuff

--- Assets/Scripts/UtilityAgent/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgent/UtilityAgent.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UtilityAgent : Agent
 {
-    Need[] needs;
+    [SerializeField] Perception perception;
+    [SerializeField] MeterUI meter;
 
-    public MeterUI meter;
+    const float MIN_SCORE = 0.1f;
+
+    Need[] needs;
+    UtilityObject activeUtilityObject = null;
+    public bool isUsingUtilityObject { get { return activeUtilityObject != null; } }
 
     public float happiness
     {
@@ -35,10 +41,99 @@ public class UtilityAgent : Agent
     void Update()
     {
         //animator.SetFloat("speed", movement.velocity.magnitude);
+        
+        if(activeUtilityObject == null)
+        {
+            var gameObjects = perception.GetGameObjects();
+            List<UtilityObject> utilityObjects = new List<UtilityObject>();
+            foreach(var go in gameObjects)
+            {
+                if(go.TryGetComponent<UtilityObject>(out UtilityObject utilityObject))
+                {
+                    utilityObject.visible = true;
+                    utilityObject.score = GetUtilityObjectScore(utilityObject);
+                    if(utilityObject.score > MIN_SCORE) utilityObjects.Add(utilityObject);
+                }
+            }
+
+            //set active utility object to first utility object
+            activeUtilityObject = (utilityObjects.Count == 0) ? null : utilityObjects[0];
+            if(activeUtilityObject != null)
+            {
+                StartCoroutine(ExecuteUtilityObject(activeUtilityObject));
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
         meter.slider.value = happiness;
         meter.worldPosition = transform.position + (Vector3.up * 4);
     }
 
+    IEnumerator ExecuteUtilityObject(UtilityObject utilityObject)
+    {
+        //go to location
+        movement.MoveTowards(utilityObject.location.position);
+        while(Vector3.Distance(transform.position, utilityObject.location.position) > 2.0f)
+        {
+            Debug.DrawLine(transform.position, utilityObject.location.position);
+
+            yield return null;
+        }
+
+        print("start effect");
+
+        //start effect
+        if (utilityObject.effect != null) utilityObject.effect.SetActive(true);
+
+        //wait duration
+        yield return new WaitForSeconds(utilityObject.duration);
+
+        //stop effect
+        if (utilityObject.effect != null) utilityObject.effect.SetActive(false);
+        print("stop effect");
+
+        //apply
+        ApplyUtilityObject(utilityObject);
+
+        activeUtilityObject = null;
+
+        yield return null;
+    }
+
+    void ApplyUtilityObject(UtilityObject utilityObject)
+    {
+        foreach (var effector in utilityObject.effectors)
+        {
+            Need need = GetNeedByType(effector.type);
+            if (need != null)
+            {
+                need.input += effector.change;
+                need.input = Mathf.Clamp(need.input, -1, 1);
+            }
+        }
+    }
+
+    float GetUtilityObjectScore(UtilityObject utilityObject)
+    {
+        float score = 0;
+        foreach(var effector in utilityObject.effectors)
+        {
+            Need need = GetNeedByType(effector.type);
+            if(need != null)
+            {
+                float futureNeed = need.getMotive(need.input + effector.change);
+                score += need.motive - futureNeed;
+            }
+        }
+        return score;
+    }
+
+    Need GetNeedByType(Need.Type type)
+    {
+        return needs.First(need => need.type == type);
+    }
     /*private void OnGUI()
     {
         Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
--- Assets/Scripts/UtilityAgent/UtilityObject.cs
+++ Assets/Scripts/UtilityAgent/UtilityObject.cs
@@ -13,19 +13,36 @@ public class UtilityObject : MonoBehaviour
 
     }
 
+    [SerializeField] MeterUI meterPrefab;
     public float duration;
     public Transform location;
+    public GameObject effect;
 
     public Effector[] effectors;
-
     public Dictionary<Need.Type, float> registry = new Dictionary<Need.Type, float>();
+
+    public float score { get; set; }
+    public bool visible { get; set; }
+    
+    MeterUI meter;
 
     void Start()
     {
+        meter = Instantiate(meterPrefab, GameObject.Find("Canvas").transform);
+        meter.name = name;
+        meter.text.text = name;
         foreach (var effector in effectors)
         {
             registry[effector.type] = effector.change;
         }
+    }
+
+    private void LateUpdate()
+    {
+        meter.gameObject.SetActive(visible);
+        meter.worldPosition = transform.position + Vector3.up * 3;
+        meter.slider.value = score;
+        visible = false;
     }
 
     public float GetEffectorChange(Need.Type type)
